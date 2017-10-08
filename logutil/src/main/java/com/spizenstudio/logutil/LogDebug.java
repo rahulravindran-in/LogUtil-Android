13959a5ea8ffc7718c6029c297c351f579eb4a53
Rahul <rahulravindran.in@gmail.com>
2017-10-08 18:36:00 +0530

    Create empty test project with Log Debug Class

---
+++ logutil/src/main/java/com/spizenstudio/logutil/LogDebug.java
@@ -0,0 +1,16 @@
+package com.spizenstudio.logutil;
+
+import android.util.Log;
+
+/**
+ * Created by rahul on 08-10-2017.
+ * From company : Spizen
+ */
+
+public class LogDebug {
+
+    public static void d(){
+        Log.d("TAGER","Lib Works!!");
+    }
+
+}
